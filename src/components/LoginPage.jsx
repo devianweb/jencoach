6e409b19f8677b9b3e9742c2ffe46257b2d3828b
ianWebsterGitHub <ian@duskwood.design>
2022-02-11 23:07:49 +0000

    added login button

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -2,8 +2,11 @@ import "./LoginPage.css";
 import React from "react";
 import bigLogo from "../resources/jc-big-logo.png";
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 const LoginPage = () => {
+  const handleClick = () => {};
+
   return (
     <div className="loginContainer">
       <div className="inputContainer">
@@ -20,8 +23,18 @@ const LoginPage = () => {
           label=""
           variant="outlined"
           type="password"
-          sx={{ backgroundColor: "white", borderRadius: "4px" }}
+          sx={{
+            backgroundColor: "white",
+            borderRadius: "4px",
+          }}
         />
+        <Button
+          variant="contained"
+          sx={{ fontFamily: "Exo 2", fontSize: "1em", marginTop: "20px" }}
+          onClick={handleClick}
+        >
+          LOGIN
+        </Button>
       </div>
     </div>
   );
